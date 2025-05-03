ec72450c157fb3ea96d192853003793a74b8a9ec
Web2Bariflo <web2.bariflo@outlook.com>
2025-05-03 16:34:32 +0530

    final update

--- src/components/pages/FeederMotor.jsx
+++ src/components/pages/FeederMotor.jsx
@@ -203,7 +203,7 @@ const FeederMotorTest = () => {
             </div>
             <div className="w-full bg-gray-200 rounded-full h-2">
               <div
-                className="bg-indigo-600 h-2 rounded-full transition-all duration-300"
+                className="bg-green-600 h-2 rounded-full transition-all duration-300"
                 style={{ width: `${remainingPercent}%` }}
               ></div>
             </div>
